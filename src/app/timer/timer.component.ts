8090a487ab95c313b6d3f849ed2843fb08eb00f5
denysLerroan <lerroandeco@gmail.com>
2020-10-17 02:17:26 -0300

    start() and pause() exercises functions

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Exercise } from '../exercise';
 
 @Component({
@@ -6,15 +6,27 @@ import { Exercise } from '../exercise';
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css'],
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent implements OnInit, OnDestroy {
   @Input() exercises: Exercise[] = [];
-  currentEx: number = 0;
-  currentRep: number = 0;
-  phase: number = 0;
+  currentEx: number;
+  currentRep: number;
+  phase: number;
+  timeLeft: number;
+  interval: NodeJS.Timeout;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.restart();
+  }
+
+  ngOnDestroy(): void {
+    this.pause();
+  }
+
+  formatTimeLeft(time: number) {
+    return (time / 10).toString();
+  }
 
   formatPhase(phase: number) {
     switch (phase) {
@@ -27,10 +39,31 @@ export class TimerComponent implements OnInit {
     }
   }
 
+  start() {
+    if (!this.interval) {
+      this.interval = setInterval(() => {
+        if (this.timeLeft > 0) {
+          this.timeLeft--;
+        } else {
+          this.next();
+        }
+      }, 100);
+    }
+  }
+
+  pause() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = undefined;
+    }
+  }
+
   restart() {
     this.currentEx = 0;
     this.currentRep = 0;
     this.phase = 0;
+    const ex = this.exercises[this.currentEx];
+    this.timeLeft = this.getTimeOfCurrentPhase();
   }
 
   next() {
@@ -47,9 +80,22 @@ export class TimerComponent implements OnInit {
           this.currentRep = 0;
           this.phase = 0;
         } else {
-          //
+          return;
         }
       }
+    }
+    this.timeLeft = this.getTimeOfCurrentPhase();
+  }
+
+  getTimeOfCurrentPhase() {
+    const ex = this.exercises[this.currentEx];
+    switch (this.phase) {
+      case 0:
+        return ex.warmUp * 10;
+      case 1:
+        return ex.duration * 10;
+      case 2:
+        return ex.rest * 10;
     }
   }
 }
